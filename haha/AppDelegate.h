70a3df63315a79aab67cf0b41ef0ff14a2b314a2
chenheard <chenheard@163.com>
2017-06-14 21:59:32 +0800

    Initial Commit

---
+++ haha/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  haha
+//
+//  Created by 陈金雷 on 2017/6/14.
+//  Copyright © 2017年 chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ haha/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  haha
+//
+//  Created by 陈金雷 on 2017/6/14.
+//  Copyright © 2017年 chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
